a1b46680e524454933266b84e8b7e4e7826c5d69
blackychris24 <blackychris24@gmail.com>
2019-10-01 02:36:45 +0900

    Upgrade pom.xml / build.gradle
    1. 각 module에서 loginAPI 의존성 추가
    2. calendar module에서 의존적 class 삭제 (> module 의존성으로 변경)

--- calendarAPI/src/main/java/com/example/daily_issue/calendar/domain/RootTask.java
+++ calendarAPI/src/main/java/com/example/daily_issue/calendar/domain/RootTask.java
@@ -1,5 +1,6 @@
 package com.example.daily_issue.calendar.domain;
 
+import com.example.daily_issue.login.domain.User;
 import org.springframework.data.jpa.domain.AbstractAuditable;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
