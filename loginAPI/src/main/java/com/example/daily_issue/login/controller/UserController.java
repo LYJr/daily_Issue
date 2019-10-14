c2b6e360d327c51cccab92eaa6179ddc9cc1bab1
이예지 <mesvie50@gmail.com>
2019-10-14 20:06:13 +0900

    ing

--- loginAPI/src/main/java/com/example/daily_issue/login/controller/UserController.java
+++ loginAPI/src/main/java/com/example/daily_issue/login/controller/UserController.java
@@ -1,22 +1,31 @@
 package com.example.daily_issue.login.controller;
 
+import com.example.daily_issue.login.domain.User;
 import com.example.daily_issue.login.domain.repository.UserRepository;
 import com.example.daily_issue.login.dto.UserDto;
+import com.example.daily_issue.login.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
-@RequestMapping("/")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private LoginService loginService;
 
-    @PostMapping ("join")
-    public String join (UserDto userDto) {
+    @PostMapping ("/login")
+    public String login (UserDto userDto) {
+        Optional<User> originUser = loginService.findByUserId(userDto.getUserId());
 
-        return null;
+        // TODO: 2019-10-14 변경중
+        if(originUser.isPresent()){
+            loginService.login(originUser.get(), userDto);
+        }
+
+        return "/index";
     }
 }
--- loginAPI/src/main/java/com/example/daily_issue/login/service/LoginService.java
+++ loginAPI/src/main/java/com/example/daily_issue/login/service/LoginService.java
@@ -2,8 +2,11 @@ package com.example.daily_issue.login.service;
 
 import com.example.daily_issue.login.domain.User;
 import com.example.daily_issue.login.domain.repository.UserRepository;
+import com.example.daily_issue.login.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class LoginService {
@@ -11,18 +14,18 @@ public class LoginService {
     @Autowired
     private UserRepository userRepository;
 
-    private User findById(String userId) {
+    public Optional<User> findByUserId(String userId) {
         return userRepository.findByUserId(userId);
     }
 
-    public boolean login(String userId, String password) {
-        User originUser = findById(userId);
-        if (originUser.idConfirm(userId)) {
+    public boolean login(User originUser, UserDto userDto) {
+        if (originUser.idConfirm(userDto.getUserId())) {
 
-            if(originUser.passwordConfirm(password)){
+            if(originUser.passwordConfirm(userDto.getPassword())){
                 return true;
             }
         }
         return false;
     }
+
 }
