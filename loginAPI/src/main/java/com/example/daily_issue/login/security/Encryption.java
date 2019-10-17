bf09bae5706a3a23d86ce0bdd59b40ad69318047
이예지 <mesvie50@gmail.com>
2019-10-17 13:12:02 +0900

    security 적용중

--- loginAPI/src/main/java/com/example/daily_issue/login/security/Encryption.java
+++ loginAPI/src/main/java/com/example/daily_issue/login/security/Encryption.java
@@ -1,5 +1,6 @@
 package com.example.daily_issue.login.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +10,11 @@ import java.util.Map;
 
 public class Encryption {
 
+    /**
+     * 다른 암호화 사용시 encoders에 추가할 것.
+     * @return
+     */
+    @Bean
     public static PasswordEncoder delegatingPasswordEncoder(){
         String encodingId = "bcrypt";
         Map<String, PasswordEncoder> encoders = new HashMap<>();
