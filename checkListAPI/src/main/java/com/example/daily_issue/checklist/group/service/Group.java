cc2ff8e8ebb04d5f3d708f2bfbc0995aca031992
오길환 <soora_genius@naver.com>
2019-10-29 23:28:17 +0900

    공통모델에 Audit 추가

--- checkListAPI/src/main/java/com/example/daily_issue/checklist/group/service/Group.java
+++ checkListAPI/src/main/java/com/example/daily_issue/checklist/group/service/Group.java
@@ -1,19 +1,18 @@
 package com.example.daily_issue.checklist.group.service;
 
 import com.example.daily_issue.checklist.common.service.CommonModel;
+import com.example.daily_issue.checklist.user.service.User;
 import lombok.*;
+import org.springframework.data.jpa.domain.AbstractAuditable;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 
 @Entity @Table(name="TBN_GROUP")
-@Getter @Setter @Builder @ToString @EqualsAndHashCode(of = "groupId")
+@Getter @Setter @Builder @ToString @EqualsAndHashCode(of = "id", callSuper = true)
 @NoArgsConstructor @AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-public class Group extends CommonModel{
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Integer groupId;        // group의 PK
+@EntityListeners({AuditingEntityListener.class})
+public class Group extends CommonModel {
     private String title;           // 제목
     private String contents;        // 내용
 }
