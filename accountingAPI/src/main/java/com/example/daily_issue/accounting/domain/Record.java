f244e83128efebc9bff3b07a84d3b36b614f5a52
Jinhwan0515 <jinhwan0515@gamil.com>
2019-11-08 23:41:46 +0900

    가계부 h2설정 ing

---
+++ accountingAPI/src/main/java/com/example/daily_issue/accounting/domain/Record.java
@@ -0,0 +1,33 @@
+package com.example.daily_issue.accounting.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "tb_record")
+@Getter @Setter
+public class Record {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private  Long recordType;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Column
+    private LocalDate recordDate;
+
+    @Column
+    private String memberName;
+
+    @Column
+    private Integer price;
+}
