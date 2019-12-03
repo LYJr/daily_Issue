7db364c2397002457b83b4c8faf99b54b93b88ef
오길환 <soora_genius@naver.com>
2019-12-03 23:26:58 +0900

    test

---
+++ notifyAPI/src/main/java/com/example/daily_issue/DailyIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.daily_issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyIssueApplication.class, args);
+    }
+
+}
